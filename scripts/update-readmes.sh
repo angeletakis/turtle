dee8cd7475795a5921453c9fc42904f58a0adb5e
Alexios Angel <aangeletakis@gmail.com>
2021-11-12 12:32:53 -0500

    Added .txt readme to scripts

--- scripts/update-readmes.sh
+++ scripts/update-readmes.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
 
-# Looks for a .github directory and outputs a markdown file
-turtle_root="~/turtle"
-dirs=( . src include lib docs scripts )
-
-for d in ${dirs[@]}; do
-  mkdwnfile="$turtle_root/$d/.github/README.md"
-  txtfile="$turtle_root/$d/README.txt"
-  [ -f "$txtfile" ]   && rm "$txtfile"
-  [ -f "$mkdwnfile" ] && pandoc --verbose --from markdown --to plain "$mkdwnfile" -o "$txtfile"
-  echo "$?"
-done
+pandoc --from markdown --to plain .github/README.md -o README.txt
